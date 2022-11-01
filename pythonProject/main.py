b15d6cc14b44a71ace4b8012f8bd4c16e21150d6
ChocoPancakes1219 <81368296+ChocoPancakes1219@users.noreply.github.com>
2022-11-01 09:42:46 +0000

    update file reading to current directory

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
 from sklearn import svm
 
-data = pd.read_csv('D:\\pythonProject\\flag.csv')
+data = pd.read_csv('../pythonProject/flag.csv')
 
 # Cleaning the data
 data = data.drop('name', axis=1)
@@ -13,7 +13,7 @@ data['mainhue'] = le.fit_transform(data['mainhue'])
 data['topleft'] = le.fit_transform(data['topleft'])
 data['botright'] = le.fit_transform(data['botright'])
 
-# Creating dummy data to classify string data
+# Separating the data
 X = data.iloc[:, 0:-1].values
 y = data. iloc [:, 1].values
 
@@ -28,6 +28,7 @@ for train_indices, test_indices in kf.split(X):
     print(clf.score(X[test_indices], y[test_indices]))
 
 print("\n")
+
 # MLP
 kf2 = KFold(n_splits=5)
 mlp = MLPClassifier(hidden_layer_sizes=(6, 4), solver='lbfgs', random_state=1, max_iter=50000)
